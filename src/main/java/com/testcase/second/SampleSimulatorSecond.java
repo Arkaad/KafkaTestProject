35e6579bfbb51382056eaf3e6f691cfffa54b69c
Arka Dutta <arka.dutta@globalids.com>
2018-02-14 18:33:41 +0530

    New Print Messages

--- src/main/java/com/testcase/second/SampleSimulatorSecond.java
+++ src/main/java/com/testcase/second/SampleSimulatorSecond.java
@@ -11,20 +11,21 @@ import java.util.concurrent.ExecutionException;
  */
 public class SampleSimulatorSecond {
     private int intervals;
+    private int limit;
     private long timeInterval;
 
-    public SampleSimulatorSecond(int intervals, long timeInterval) {
+    public SampleSimulatorSecond(int intervals, long timeInterval, int limit) {
         this.intervals = intervals;
         this.timeInterval = timeInterval;
+        this.limit = limit;
     }
 
     public void startProcess() throws ExecutionException, InterruptedException {
-        long safeTime = 20 * 1000; //10 secs
+        long safeTime = 20 * 1000; //20 secs
         long windowTime = (timeInterval * (intervals - 1)) + safeTime;
         System.out.println("windowTime = " + windowTime + " ms.");
 
         Producer producer;
-        int limit = 1000000; //1 million
         long startOffset = 0L;
         long endOffset = 0L;
         JoinStreamSecond stream = new JoinStreamSecond(windowTime);
@@ -41,11 +42,14 @@ public class SampleSimulatorSecond {
             System.out.println("Publishing Data ......");
             long startPublish = System.currentTimeMillis();
             int j = 1;
-            startOffset = producer.publishData(String.valueOf(j), producer.getClass().getSimpleName() + "_" + j);
+            startOffset = producer.publishData(("ABCDEFGHIJKLMNOP123456QRST458692_25896314784569321478956321478" + String.valueOf(j)), producer.getClass().getSimpleName() + "_" + j);
             for (; j <= limit + (i * 5); j++) {
-                endOffset = producer.publishData(String.valueOf(j), producer.getClass().getSimpleName() + "_" + j);
+                endOffset = producer.publishData(("ABCDEFGHIJKLMNOP123456QRST458692_25896314784569321478956321478" + String.valueOf(j)), producer.getClass().getSimpleName() + "_" + j);
+                if (j % 5000 == 0) {
+                    System.out.println(j + " data Published");
+                }
             }
-            System.out.println("End of Publishing data, Time taken : " + (System.currentTimeMillis() - startPublish) + " ms.");
+            System.out.println("End of Publishing " + (j - 1) + " data, Time taken : " + (System.currentTimeMillis() - startPublish) + " ms.");
             producer.close();
             Thread.sleep(2 * 1000);
             if (i > 0 && ((i + 1) != intervals)) {
